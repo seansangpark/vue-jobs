1a44ab83610ea18b88555ef8caed286e116ff594
Sean Park <seansangpark@gmail.com>
2024-08-08 14:36:47 -0400

    vue working

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,19 @@
+<script setup>
+import Navbar from '@/components/Navbar.vue';
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+import JobListings from '@/components/JobListings.vue';
+</script>
+
 <template>
-  <h1>Vue Jobs</h1>
+  <Navbar />
+  <Hero
+    title="Test Title"
+    subtitle="Test Subtitle"
+  />
+  <HomeCards />
+  <JobListings
+    :limit="3"
+    :showButton="true"
+  />
 </template>
