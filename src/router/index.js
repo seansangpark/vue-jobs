bff75f0ecbf0c2b59b64ca5c8fa591a39f9a68b4
Sean Park <seansangpark@gmail.com>
2024-08-08 16:11:44 -0400

    mock restAPI

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 import JobsViews from '@/views/JobsViews.vue';
 import NotFoundView from '@/views/NotFoundView.vue';
+import JobView from '@/views/JobView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/jobs',
       name: 'jobs',
       component: JobsViews,
+    },
+    {
+      path: '/jobs/:id',
+      name: 'job',
+      component: JobView,
     },
     {
       path: '/:catchAll(.*)',
