ac8d49be28c40a016e1570c03127ced8033848f3
Sean Park <seansangpark@gmail.com>
2024-08-09 13:52:50 -0400

    deleting jobs

--- src/views/AddJobView.vue
+++ src/views/AddJobView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import router from '@/router';
 import { reactive } from 'vue';
+import { useToast } from 'vue-toastification';
 import axios from 'axios';
 
 const form = reactive({
@@ -16,6 +17,8 @@ const form = reactive({
     contactPhone: '',
   },
 });
+
+const toast = useToast();
 
 const handleSubmit = async () => {
   const newJob = {
@@ -34,11 +37,11 @@ const handleSubmit = async () => {
 
   try {
     const response = await axios.post('/api/jobs/', newJob);
-    // @todo - show toast
+    toast.success('Job Added Successfully');
     router.push(`/jobs/${response.data.id}`);
   } catch (error) {
-    // @todo - show toast
     console.error('Error posting job', error);
+    toast.error('Job Was Not Added');
   }
 };
 </script>
